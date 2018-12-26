1db57d90b48c88ac8a31695eedfb3a290589ca0f
Daniel Eubanks <jdanieleubanks@gmail.com>
2018-12-26 17:09:24 -0600

    m

--- markov_text_nytimes.R
+++ markov_text_nytimes.R
@@ -66,7 +66,7 @@ for(url in urls) {
 }
 
 # Convert to JSON; use text editor to adjust encoding
-wordListJSON <- toJSON(lapply(wordList, as.list),pretty=TRUE,unbox=TRUE,encoding="UTF8")
+wordListJSON <- toJSON(lapply(wordList, as.list),pretty=TRUE,unbox=TRUE)
 write(wordListJSON,"nytimes_wordlist.json")
 
 # Save RData version
